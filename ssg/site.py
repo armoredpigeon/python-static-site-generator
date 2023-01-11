6fc762c5e167bd51ef90a07c97209e17069ac135
armoredpigeon <mjonesartand3d@gmail.com>
2023-01-10 21:44:07 -0700

    the site class

---
+++ ssg/site.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import os
+
+class Site:
+  def __init__(self, source, dest) -> None:
+    self.source = Path(source)
+    self.dest = Path(dest)
+
+  def create_dir(self, path):
+    directory = f'{self.dest}/{path.relative_to(self.source)}'
+    directory.mkdir(parents=True, exist_ok=True)
+    
+  def build(self):
+    self.dest.mkdir(parents=True, exist_ok=True)
+    for path in self.source.rglob("*"):
+      if(os.path.isfile(path)):
+        self.create_dir(path)
+
+  
